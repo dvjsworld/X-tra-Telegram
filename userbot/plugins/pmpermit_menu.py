8ad86f2d98f7b915aeaa29c734fc2638ff3dc51f
dvjsworld <37199396+dvjsworld@users.noreply.github.com>
2020-01-24 23:10:34 +0530

    Update pmpermit_menu.py

--- userbot/plugins/pmpermit_menu.py
+++ userbot/plugins/pmpermit_menu.py
@@ -34,20 +34,19 @@ async def _(event):
                   "`3`. Other\n")
          PM = ("`Hello. You are accessing the availabe menu of my peru master,`"
                f"{DEFAULTUSER}.\n"
-               "__Let's make this smooth and let me know why you are here.__\n"
-               "**Choose one of the following reasons why you are here:**\n\n"
+               "__visit http://telegramguru.online for new group links .__\n"
+               "**active groups added daily:**\n\n"
                "`1`. To chat with my master\n"
                "`2`. To spam my master's inbox.\n"
                "`3`. To send nudes.\n"
                "`4`. To enquire something\n"
                "`5`. To request something\n")
-         ONE = ("__Okay. Your request has been registered. Do not spam my master's inbox.You can expect a reply within 24 light years. He is a busy man, unlike you probably.__\n\n"
-                "**⚠️ You will be blocked and reported if you spam nibba. ⚠️**\n\n"
+         ONE = ("__Okay.visit http://telegramguru.online for new group links  ⚠️**\n\n"
                 "__Use__ `/start` __to go back to the main menu.__")
-         TWO = (" `███████▄▄███████████▄  \n▓▓▓▓▓▓█░░░░░░░░░░░░░░█\n▓▓▓▓▓▓█░░░░░░░░░░░░░░█\n▓▓▓▓▓▓█░░░░░░░░░░░░░░█\n▓▓▓▓▓▓█░░░░░░░░░░░░░░█\n▓▓▓▓▓▓█░░░░░░░░░░░░░░█\n▓▓▓▓▓▓███░░░░░░░░░░░░█\n██████▀▀▀█░░░░██████▀  \n░░░░░░░░░█░░░░█  \n░░░░░░░░░░█░░░█  \n░░░░░░░░░░░█░░█  \n░░░░░░░░░░░█░░█  \n░░░░░░░░░░░░▀▀ `\n\n**So uncool, this is not your home. Go bother someone else. You have been blocked and reported until further notice.**")
-         FOUR = ("__Okay. My master has not seen your message yet.He usually responds to people,though idk about retarted ones.__\n __He'll respond when he comes back, if he wants to.There's already a lot of pending messages😶__\n **Please do not spam unless you wish to be blocked and reported.**")
-         FIVE = ("`Okay. please have the basic manners as to not bother my master too much. If he wishes to help you, he will respond to you soon.`\n**Do not ask repeatdly else you will be blocked and reported.**")
-         LWARN = ("**This is your last warning. DO NOT send another message else you will be blocked and reported. Keep patience. My master will respond you ASAP.**\n__Use__ `/start` __to go back to the main menu.__")
+         TWO = visit http://telegramguru.online for new group links `\n\n**So uncool, this is not your home. Go bother someone else. You have been blocked and reported until further notice.**")
+         FOUR = ("__Okay. visit http://telegramguru.online for new group links.*")
+         FIVE = ("`Okay. visit http://telegramguru.online for new group links**)
+         LWARN = ("**This is your last warning visit http://telegramguru.online for new group links.__")
      
         async with borg.conversation(chat) as conv:
          await borg.send_message(chat, PM)
